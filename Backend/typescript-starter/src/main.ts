5bcaa3b1f15299e4d01aa42c333297ce6572d50d
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2026-09-18 10:46:21 +0200

    Images

--- Backend/typescript-starter/src/main.ts
+++ Backend/typescript-starter/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { join } from 'node:path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+app.useStaticAssets(join(__dirname, '..', 'public'));
+app.enableCors();
+await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
